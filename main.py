713db53b62a99a4ba0e48d89371bfe304324c7b3
Alexander White <alexanderb.white1@gmail.com>
2020-06-09 05:51:30 -0400

    Add main function

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello world!")
-for n in range(10):
-    print(f"Number {n} cubed: {n**3}")
-    print(f"Number {n} to the fifth: {n**5}")
+def main():
+    print("Hello world!")
+    for n in range(10):
+        print(f"Number {n} cubed: {n**3}")
+        print(f"Number {n} to the fifth: {n**5}")
+
+if __name__ == '__main__':
+    main()
