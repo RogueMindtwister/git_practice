b2e3ed7fc278b4ed9755c191cfef091af92411cb
Alexander White <alexanderb.white1@gmail.com>
2020-06-11 01:22:45 -0400

    Add finance methods

---
+++ finance.py
@@ -0,0 +1,5 @@
+def get_interest_rate():
+    return "Interest rate is 2%"
+
+def get_interest(amount, interest_rate):
+    return amount * interest_rate
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from finance import *
+
 def main():
     print("Hello world!")
     for n in range(10):
         print(f"Number {n} cubed: {n**3}")
         print(f"Number {n} to the fifth: {n**5}")
+
+    print(get_interest_rate())
+    print(get_interest(1375.50, 0.02))
 
 if __name__ == '__main__':
     main()
