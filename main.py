4ffb6f1e484866bd3375e9036673b4186b9129a9
Alexander White <alexanderb.white1@gmail.com>
2020-06-13 04:43:40 -0400

    Add user input

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def main():
     print("Hello world!")
-    for n in range(10):
+    num = int(input("Please enter a number between 1 and 25: "))
+    while num < 1 or num > 25:
+        num = int(input("Invalid number entered, must be between 1 and 25. Enter again: "))
+    for n in range(1, num+1):
         print(f"Number {n} cubed: {n**3}")
         print(f"Number {n} to the fifth: {n**5}")
+
+
 
 if __name__ == '__main__':
     main()
