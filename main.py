0bd307a9d57c12a6abed95c224a88fd42e03e45f
Alexander White <alexanderb.white1@gmail.com>
2020-06-17 21:32:46 -0400

    Add comprehensions function

--- main.py
+++ main.py
@@ -9,12 +9,19 @@ def main():
 
     print("------FACTORIAL TESTING-----")
     print(fact(8))
-
+    comprhensions()
 
 def fact(n):
     if n <= 1:
         return 1
     return n * fact(n-1)
+
+# A function that demonstrates list comprehensions
+def comprhensions():
+    numbers = [1, 5, 6, 73, 3, 32, 16, 7, 55, 21, 75]
+
+    altered_numbers = [number ** 2 for number in numbers]
+    print(altered_numbers)
 
 if __name__ == '__main__':
     main()
